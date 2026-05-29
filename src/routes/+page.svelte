d8f435d02a3b47c446ddf0455bbcb729a71ff61b
naroah <givefacegod@gmail.com>
2026-05-29 14:43:16 +0800

    update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import Calendar from '$lib/calendar/Calendar.svelte';
+	import SpeechText from '$lib/SpeechText.svelte';
 </script>
 
-<main>
-	<Calendar />
+<main class="flex min-h-screen w-full gap-8">
+	<section class="flex-1">
+		<SpeechText />
+	</section>
+	<section class="flex-1">
+		<Calendar />
+	</section>
 </main>
