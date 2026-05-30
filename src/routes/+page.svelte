d894ba1798e3002ca6b803ce77192dca31a257d2
naroah <givefacegod@gmail.com>
2026-05-30 09:11:54 +0800

    update routes page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,13 @@
 	let pendingEvent = $state<Event | null>(null);
 	let calendar = $state<Calendar>();
 
+	function getEventId(data: Event): string {
+		function get_time_key(t: Time): string {
+			return [t.year, t.month, t.day, t.hour, t.minute, t.second].join('-');
+		}
+		return [data.title, get_time_key(data.start), get_time_key(data.end)].join('|');
+	}
+
 	function toScheduleXEvent(data: Event): CalendarEventExternal {
 		function get_zdt(t: Time): Temporal.ZonedDateTime {
 			const pad = (value: number): string => String(value).padStart(2, '0');
@@ -18,7 +25,7 @@
 			return Temporal.ZonedDateTime.from(value + zone);
 		}
 		return {
-			id: crypto.randomUUID(),
+			id: getEventId(data),
 			title: data.title,
 			start: get_zdt(data.start),
 			end: get_zdt(data.end),
@@ -26,9 +33,19 @@
 			location: data.location ? data.location : undefined
 		};
 	}
-	function handleConfirm(): void {
-		if (!pendingEvent) return;
-		calendar?.addEvent(toScheduleXEvent(pendingEvent));
+
+	function handleCreate(data: Event): void {
+		calendar?.addEvent(toScheduleXEvent(data));
+		pendingEvent = null;
+	}
+
+	function handleDelete(data: Event): void {
+		calendar?.removeEvent(getEventId(data));
+		pendingEvent = null;
+	}
+
+	function handleUpdate(data: Event): void {
+		calendar?.updateEvent(toScheduleXEvent(data));
 		pendingEvent = null;
 	}
 </script>
@@ -36,15 +53,18 @@
 <main class="flex w-full gap-8">
 	<section class="flex flex-1 flex-col gap-4">
 		<SpeechText onEventRecognized={(data) => (pendingEvent = data)} />
-		{#if pendingEvent}
-			<ScheduleConfirm
-				data={pendingEvent}
-				onCancel={() => (pendingEvent = null)}
-				onConfirm={() => handleConfirm()}
-			/>
-		{/if}
 	</section>
 	<section class="flex-1">
 		<Calendar bind:this={calendar} />
 	</section>
 </main>
+
+{#if pendingEvent}
+	<ScheduleConfirm
+		data={pendingEvent}
+		onCancel={() => (pendingEvent = null)}
+		onCreate={handleCreate}
+		onDelete={handleDelete}
+		onUpdate={handleUpdate}
+	/>
+{/if}
