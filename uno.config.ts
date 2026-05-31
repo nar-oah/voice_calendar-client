279537aef74de5a09280b787d7ced2feaf36e2de
naroah <givefacegod@gmail.com>
2026-05-31 20:09:24 +0800

    fix: safelist confirm action colors

--- uno.config.ts
+++ uno.config.ts
@@ -2,6 +2,19 @@ import extractorSvelte from '@unocss/extractor-svelte';
 import { defineConfig, presetWind3 } from 'unocss';
 
 export default defineConfig({
-  presets: [presetWind3()],
-  extractors: [extractorSvelte()]
+	presets: [presetWind3()],
+	extractors: [extractorSvelte()],
+	safelist: [
+		'bg-zinc-950',
+		'b-zinc-950',
+		'hover:bg-zinc-800',
+		'bg-red-600',
+		'b-red-600',
+		'hover:bg-red-700',
+		'bg-amber-500',
+		'b-amber-500',
+		'hover:bg-amber-400',
+		'text-white',
+		'text-zinc-950'
+	]
 });
